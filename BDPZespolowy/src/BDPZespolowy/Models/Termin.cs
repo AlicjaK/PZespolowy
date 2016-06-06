547b88604cd425ba0a4f69a172038a08b9d02c19
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:33:26 +0200

    Dodanie modelu Termin

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Termin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Termin
+    {
+        public int Termin_PKid { get; set; }
+        public string Termin_Od { get; set; }
+        public string Termin_Do { get; set; }
+        public int Termin_OdGodziny { get; set; }
+        public int Termin_DoGodziny { get; set; }
+    }
+}
