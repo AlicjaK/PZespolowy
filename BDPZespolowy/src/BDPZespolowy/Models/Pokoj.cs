d89351bc465c81befc86463b6112fc7893943879
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:35:14 +0200

    Dodanie modelu Pokoj

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Pokoj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Pokoj
+    {
+        public int Pokój_Pkid { get; set; }
+        public int Pokój_Numer { get; set; }
+        public bool Pokój_Dostępność { get; set; }
+        public int Pokój_IlośćMiejsc { get; set; }
+        public string Pokój_Typ { get; set; }
+    }
+}
