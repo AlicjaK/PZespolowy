13b6975c923212350a0b8bcf8608928ea51858dc
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:31:11 +0200

    Dodanie modelu Stanowisko

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Stanowisko.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Stanowisko
+    {
+        public int Stanowisko_PKid { get; set; }
+        public string Stanowisko_Nazwa { get; set; }
+        public int Stanowisko_LPracowników { get; set; }
+        public float Stanowisko_Wypłata { get; set; }
+    }
+}
