1071ff7894d20afc9e8f5ddc8de44c754d0a57bc
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:35:47 +0200

    Dodanie modelu Raporty

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Raporty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Raporty
+    {
+        public int Raporty_PKid { get; set; }
+        public int Rezerwacja_Fkid { get; set; }
+        public string Raporty_Typ { get; set; }
+        public int Raporty_LZarobionychPieniędzy { get; set; }
+    }
+}
