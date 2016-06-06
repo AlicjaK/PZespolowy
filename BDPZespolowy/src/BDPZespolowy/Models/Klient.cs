57e42c8c20130bc4e7de5cc8fb8336a3406d9479
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:37:11 +0200

    Dodanie modelu Klient

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Klient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Klient
+    {
+        public int Klient_PKid { get; set; }
+        public int Zniżki_FKid { get; set; }
+        public string Klient_Imie { get; set; }
+        public string Klient_Nazwisko { get; set; }
+        public int Klient_Pesel { get; set; }
+        public int Klient_Nrtelefonu { get; set; }
+    }
+}
