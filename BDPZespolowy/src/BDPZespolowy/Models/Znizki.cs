ab58a688de8081168188427636e3efc6430b46ed
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:36:31 +0200

    Dodanie modelu Znizki

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Znizki.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Znizki
+    {
+        public int Znizki_PKid { get; set; }
+        public int Znizki_LPunktow { get; set; }
+        public string Znizki_ProcZnizki { get; set; }
+    }
+}
