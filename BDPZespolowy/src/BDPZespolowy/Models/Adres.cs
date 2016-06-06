a94aca2ab2360a9b3c526cc582ef736a7c96c88c
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:32:41 +0200

    Dodanie modelu Adres

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Adres.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Adres
+    {
+        public int Adres_PKid { get; set; }
+        public string Adres_Kraj { get; set; }
+        public string Adres_Ulica { get; set; }
+        public int Adres_KodPocztowy { get; set; }
+        public string Adres_Miasto { get; set; }
+        public int Adres_Nrmieszkania { get; set; }
+        public int Adres_Nrdomu { get; set; }
+    }
+}
