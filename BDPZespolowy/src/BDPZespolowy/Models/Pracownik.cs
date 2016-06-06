44a40c08d7e1cee87150aa990ce60f91f966c2e0
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:32:07 +0200

    Dodanie modelu Pracownik

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Pracownik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Pracownik
+    {
+
+        public int Pracownik_PKid { get; set; }
+        public int Adres_FKid { get; set; }
+        public decimal Stanowisko_FKid { get; set; }
+        public string Pracownik_Imie { get; set; }
+        public string Pracownik_Nazwisko { get; set; }
+        public int Pracownik_Pesel { get; set; }
+        public int Pracownik_Nrtelefonu { get; set; }
+
+    }
+}
