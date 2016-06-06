6deed9e4babd851c084cb6170c0c1cbf2ac7de35
Alicja Kula <alicja.j.kula@gmail.com>
2016-06-07 00:34:21 +0200

    Dodanie modelu Cena

---
+++ BDPZespolowy/src/BDPZespolowy/Models/Cena.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDPZespolowy.Models
+{
+    public class Cena
+    {
+        public int Cena_Pkid { get; set; }
+        public float Cena_Netto { get; set; }
+        public float Cena_Vat { get; set; }
+    }
+}
